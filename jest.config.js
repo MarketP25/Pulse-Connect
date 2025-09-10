b08804fd04449893d657254676a68a23e77f19bc
Market Pulse Pro <marketpulsepro25@gmail.com>
2025-09-10 10:26:14 +0300

    Skip tests for now

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,17 @@
+// jest.config.js
 export default {
   testEnvironment: "jest-environment-jsdom",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   moduleNameMapper: {
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
-    "^@/(.*)$": "<rootDir>/src/$1"
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "^(\\.{1,2}/.*)\\.js$": "$1"
   },
   transform: {
-    "^.+\\.tsx?$": "babel-jest",
-    "^.+\\.jsx?$": "babel-jest"
+    "^.+\\.(ts|tsx|js|jsx)$": [
+      "babel-jest",
+      { configFile: "./babel.config.cjs" }
+    ]
   },
-  extensionsToTreatAsEsm: [".ts", ".tsx"],
+  extensionsToTreatAsEsm: [".ts", ".tsx"]
 };
